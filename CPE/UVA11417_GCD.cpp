6908258efcee9350045beb7ed503e326cf4f608e
Willy <willyhome85@gmail.com>
2018-07-26 13:32:47 +0800

    Initial commit

---
+++ CPE/UVA11417_GCD.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int gcd(int n, int m)
+{
+    if(m == 0)
+    {
+        return n;
+    }
+    return gcd(m, n%m);
+}
+
+int main()
+{
+    int n, total;
+
+    while(cin >> n && n !=0)
+    {
+        total = 0;
+        for(int i=1; i<n; ++i)
+        {
+            for(int j=i+1; j<=n; ++j)
+            {
+                total += gcd(i, j);
+            }
+        }
+
+        cout << total << endl;
+    }    
+    
+    return 0;
+}
