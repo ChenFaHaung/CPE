e91b54952b6bb74841697736d3bddd1d9f92481b
Willy <willyhome85@gmail.com>
2018-08-23 13:36:12 +0800

    Determine the numbers of square number

---
+++ CPE/UVA11461_SquareNumbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    while(cin >> a >> b)
+    {
+        int c = 0;
+        double d, e;
+        if(a == 0 && b == 0)
+        {
+            break;
+        }
+        if(a > b)
+        {
+            swap(a, b);
+        }
+        d = sqrt(a);
+        e = sqrt(b);
+
+        cout << floor(e) - ceil(d) + 1 << endl;
+    }
+}
