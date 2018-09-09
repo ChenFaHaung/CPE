fbacd15210adf95a476ab2f85278b2cf6a8a42fd
Willy <willyhome85@gmail.com>
2018-09-09 20:01:12 +0800

    String reverse and KMP

---
+++ CPE/UVA11475_ExtendToPalin.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#define LEN 100001
+
+int main()
+{
+    char s[LEN];
+    char p[LEN];
+
+    while(gets(s))
+    {
+        int fail[LEN] = {-1}, len = strlen(s),i,j;
+        reverse_copy(s,s+len, p);
+
+        for(i=1, j=-1; i<len; ++i)
+        {
+            while(j >=0 && p[j+1] != p[i])
+                j = fail[j];
+            if(p[j+1] == p[i])
+                ++j;
+            fail[i] = j;
+        }
+
+        for(i=0, j=-1; i<len; ++i)
+        {
+            while(j>=0 && p[j+1] != s[i])
+                j = fail[j];
+            if(p[j+1] == s[i])
+                ++j;
+        }
+
+        for(i=0; i<len; ++i)
+            putchar(s[i]);
+        for(++j; j<len; ++j)
+            putchar(p[j]);
+        putchar('\n');
+    }
+
+    return 0;
+}
