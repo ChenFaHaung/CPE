4368729801e654ec9aa9e6a24424ff4933c702fe
Willy <willyhome85@gmail.com>
2018-09-19 18:31:15 +0800

    Easy calculate

---
+++ CPE/UVA12015_GoogleisFeeling.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    while(cin >> a)
+    {
+        for(int i=0; i<a; ++i)
+        {
+            int m = i+1;
+            string str[10];
+            int b[10], Max = 0;
+            for(int j=0; j<10; ++j)
+            {
+                cin >> str[j] >> b[j];
+                if(b[j] > Max)
+                    Max = b[j];
+            }
+            cout << "Case #" << m++ << ":\n";
+            for(int j=0; j<10; ++j)
+            {
+                if(b[j] == Max)
+                {
+                    cout << str[j] << endl;
+                }
+            }
+        }
+    }
+}
