77d73b557104ab6207c63783449689c3cd120c05
Willy <willyhome85@gmail.com>
2018-09-26 19:20:05 +0800

    Regular pattern and Big Integar add

---
+++ CPE/UVA10162_LastDigit.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+int rule[20] = {0, 1, 5, 2, 8, 3, 9, 2, 8, 7,
+                7, 8, 4, 7, 3, 8, 4, 1, 5, 4};
+
+int main()
+{
+    string str;
+
+    while(cin >> str)
+    {
+        int len = str.length();
+        if(str[0] == '0' && len == 1)
+            break;
+
+        int term = str[len - 1] - '0';
+        if(len > 1)
+            term += (str[len - 2] - '0') * 10;
+            
+        cout << (rule[term%20] + term/20*4) % 10 << endl;
+    }
+    return 0;
+}
