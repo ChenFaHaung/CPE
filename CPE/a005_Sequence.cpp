b67a8166121ee53e6d4c6d56b5b48b556a9d719d
Willy <willyhome85@gmail.com>
2018-09-26 22:07:01 +0800

    Sequence check

--- CPE/a005_Sequence.cpp
+++ CPE/a005_Sequence.cpp
@@ -14,9 +14,9 @@ int main()
         for(int i=0; i<a; ++i)
         {
             cin >> a1 >> a2 >> a3 >> a4;
-            if((a2 - a1) == (a3 - a2) && (a3 - a2) == (a4 - a3))
+            if((a2 - a1) == (a3 - a2) && (a3 - a2) == (a4 - a3)) // same difference
                 cout << a1 << " " << a2 << " " << a3 << " " << a4 << " " << a1+4*(a2-a1) << endl;
-            else
+            else // same multiple
                 cout << a1 << " " << a2 << " " << a3 << " " << a4 << " " << a1*pow((a2/a1), 4) << endl;
             
         }
