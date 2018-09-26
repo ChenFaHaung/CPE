91935e1fe93fc9d4caf1e49f429f7ab7baaa8e4b
Willy <willyhome85@gmail.com>
2018-09-26 22:02:09 +0800

    Map, sprintf, scanf

--- CPE/UVA11286_Conformity.cpp
+++ CPE/UVA11286_Conformity.cpp
@@ -9,20 +9,23 @@ int main() {
 	char buf[20];
 	while(scanf("%d", &n) == 1 && n) {
 		string str;
-		map<string, int> record;
-		while(n--) {
-			scanf("%d %d %d %d %d", &A[0], &A[1], &A[2], &A[3], &A[4]);
-			sort(A, A+5);
-			sprintf(buf, "%d%d%d%d%d", A[0], A[1], A[2], A[3], A[4]);
+		map<string, int> record; // map
+		while(n--) 
+		{
+			scanf("%d %d %d %d %d", &A[0], &A[1], &A[2], &A[3], &A[4]); // scanf each row
+			sort(A, A+5); // sort them
+			sprintf(buf, "%d%d%d%d%d", A[0], A[1], A[2], A[3], A[4]); // copy to string
 			str = buf;
-			record[str]++;
+			record[str]++; // map counting
 		}
+
 		int max = 0, maxNum = 0;
-		for(map<string, int>::iterator i = record.begin(); i != record.end(); i++) {
-			if(i->second > max)
-				max = i->second, maxNum = 0;
+		for(map<string, int>::iterator i = record.begin(); i != record.end(); i++) 
+		{ // iterator memory
+			if(i->second > max) // i->first: string 
+				max = i->second, maxNum = 0; // if some cnt over max, maxNum must rerecord
 			if(i->second == max)
-				maxNum += max;
+				maxNum += max; // if max number same, you have to accumulate the total number 
 		}
 		printf("%d\n", maxNum);
 	}
