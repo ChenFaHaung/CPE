02961c259246f6e401e181e505e78e95e2313a07
Willy <willyhome85@gmail.com>
2018-09-26 19:45:46 +0800

    Midium integer finding

--- CPE/UVA10057_AMidSummerNight.cpp
+++ CPE/UVA10057_AMidSummerNight.cpp
@@ -10,16 +10,16 @@ int main()
     int a;
     while(scanf("%d", &a) != EOF)
     {
-        int b[65537];
+        int b[65537]; // question require
         for(int i=0; i<a; ++i)
         {
             cin >> b[i];
         }
 
-        sort(b, b+a);
+        sort(b, b+a); // sort the input for finding middium
 
-        int m = b[(a-1)/2];
-        int M = b[a/2];
+        int m = b[(a-1)/2]; // find lower mid
+        int M = b[a/2]; // find mid
         int cnt = 0;
 
         for(int i=0; i<a; ++i)
@@ -27,7 +27,7 @@ int main()
             if(b[i] == m || b[i] == M)
                 ++cnt;
         }
-        cout << m << " " << cnt << " " << M-m+1 << endl;
+        cout << m << " " << cnt << " " << M-m+1 << endl; // print lower one and cal. the possible mid
 
     }
 }
