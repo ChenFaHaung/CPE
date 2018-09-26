4182a5227d72fe83df3e4dfb72540e695bbe7332
Willy <willyhome85@gmail.com>
2018-09-26 19:47:11 +0800

    Easy formula

--- CPE/UVA10071_HighSchoolPhysics.cpp
+++ CPE/UVA10071_HighSchoolPhysics.cpp
@@ -7,10 +7,10 @@ int main(){
 int v, t, displace;
 
 while(scanf("%d %d", &v, &t) != EOF){
-    if(v < -100 || v > 100 || t < 0 || t > 200){
+    if(v < -100 || v > 100 || t < 0 || t > 200){ // check the limitation
         break;
     }
-    displace = v * t * 2;
+    displace = v * t * 2; // displacement formula
     printf("%d\n", displace);
 }
 
