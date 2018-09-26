85ded4509a658ea3175b4a96b014a393bb2b2b7b
Willy <willyhome85@gmail.com>
2018-09-26 19:32:11 +0800

    %.4f, pow, prob function

--- CPE/UVA10056_WatIsTheProb.cpp
+++ CPE/UVA10056_WatIsTheProb.cpp
@@ -12,17 +12,17 @@ int main()
     while(scanf("%d", &c)!=EOF){
         for(int i=0; i<c; ++i)
         {
-            cin >> N >> p >> I;
+            cin >> N >> p >> I; // N: people enter the game; p: win probability; I: the winner 
         
         
         double suc;
         suc = pow((1-p), (I-1)) * p;
 
-        double last = 0;
-            while (suc - last > 1e-7) {
-                I += N;
+        double last = 0; // last probability
+            while (suc - last > 1e-7) { // probability less than 1e-7
+                I += N; // if no fit the rule, add the players
                 last = suc;
-                suc += pow(1-p, I-1) * p;
+                suc += pow(1-p, I-1) * p; 
             }
 
             // Print the result
