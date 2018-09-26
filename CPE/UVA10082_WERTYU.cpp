b229a190ad4987a918eaa19dee6fb41c07b87455
Willy <willyhome85@gmail.com>
2018-09-26 19:51:05 +0800

    Keyboard shift, getchar, %c,

--- CPE/UVA10082_WERTYU.cpp
+++ CPE/UVA10082_WERTYU.cpp
@@ -9,24 +9,23 @@ int main()
     char c;
     string str = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
 
-    while(c = getchar())
+    while(c = getchar()) // char analysis
     {
-        if (c== EOF ) break;
+        if (c == EOF ) break;
         int i=0;
         //cout << c;
-        if(c == ' ' || c == '\n')
+        if(c == ' ' || c == '\n') // space and line space
         {
             printf("%c",c);
-            //cout<<"123\n";
         }
         else
         {
-            while(str[i] != c)
+            while(str[i] != c) // scan the str until matching c
             {
                 ++i;
             }       
 
-            printf("%c",str[i-1]);     
+            printf("%c",str[i-1]); // print the one before str
         }
         
     }
