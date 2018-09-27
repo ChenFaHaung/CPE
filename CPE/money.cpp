5fab11d02c9fb405fda88d4ca79d1a54a102ca7b
Willy <willyhome85@gmail.com>
2018-09-27 22:31:57 +0800

    Exhausive the money set

--- CPE/money.cpp
+++ CPE/money.cpp
@@ -7,12 +7,31 @@ int main(){
 
 int money, temp = 0, ten = 10, five = 5, one = 1;
 cin >> money;
-for(int i = 0; ten*i <= money; i++){//十元拿幾個
+for(int i = 0; ten * i <= money; i++)
+{ // how many 10
     temp = money - ten * i;
-    for(int h = 0; five* h <= temp; h++){//五元拿幾個
+    for(int h = 0; five * h <= temp; h++)
+    {// how many 5
         one = temp - five * h;
         cout << "10:" << i << " 5:" << h << " 1:" << one << endl;
     }
 }
+/*char str[] = "Hello world";
+char *p = str;
+short a; 
+int b; 
+long c; 
+float d; 
+double e;
+
+printf("%d\n", sizeof(str));
+printf("%d\n", sizeof(*p));
+printf("%d\n", sizeof(p));
+printf("%d\n", sizeof(a));
+printf("%d\n", sizeof(b));
+printf("%d\n", sizeof(c));
+printf("%d\n", sizeof(d));
+printf("%d\n", sizeof(e));*/
+
 return 0;
 }
