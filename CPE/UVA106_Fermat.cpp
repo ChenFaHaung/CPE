affe54b7816e884570d349625761f6dffdcf2e76
Willy <willyhome85@gmail.com>
2018-09-28 18:55:21 +0800

    Math for Eular function

--- CPE/UVA106_Fermat.cpp
+++ CPE/UVA106_Fermat.cpp
@@ -6,6 +6,7 @@ short int p[1000001];
 
 int gcd(int m, int n)
 {
+    //return n == 0 ? m : gcd(n, m%n);
     int k;
     while (k = m % n)
     {
@@ -23,21 +24,21 @@ int main()
         int numTuple = 0; //number of tuple(a,b,c)
         memset(p, 0, sizeof(p));
 
-        for (int x = 1; x < 1000; ++x)
+        for (int x = 1; x < 1000; ++x) // sqrt 1000000
         {
             for (int y = x + 1;; y += 2)
             {
                 if (gcd(x, y) != 1) continue;
 
                 int a, b, c; //tuple(a,b,c)
-                a = y * y - x * x;
+                a = y * y - x * x; // 
                 b = 2 * x * y;
                 c = y * y + x * x;
-                if (c > max) break;
+                if (c > max) break; // break condition
 
                 numTuple++;
 
-                int ma = a, mb = b, mc = c;
+                int ma = a, mb = b, mc = c; // multiple
                 while (mc <= max)
                 {
                     p[ma] = p[mb] = p[mc] = 1;
