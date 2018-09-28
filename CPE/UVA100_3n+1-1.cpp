8ca4e2cfcaa33c8e17b3944b4762eb50b17d407c
Willy <willyhome85@gmail.com>
2018-09-28 18:47:31 +0800

    Fast ?: function

--- CPE/UVA100_3n+1-1.cpp
+++ CPE/UVA100_3n+1-1.cpp
@@ -15,14 +15,14 @@ int main(void) {
     long i, j, min_i_j, max_i_j;
     long max_length, term_length;
 
-    while (scanf("%ld %ld", &i, &j) != EOF) {
+    while (scanf("%ld %ld", &i, &j) != EOF) { // may be long
         max_length = 0;
 
         // Get the greatest and lowest value in i and j
         min_i_j = (i < j) ? i : j;
         max_i_j = (i > j) ? i : j;
 
-        while (min_i_j <= max_i_j) {
+        while (min_i_j <= max_i_j) { // amazing
             term_length = cycleLength(min_i_j);
             max_length = (term_length > max_length) ? term_length : max_length;
             ++min_i_j;
