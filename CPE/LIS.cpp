15642338070c3847efb163d506f05f00c2ac8566
Willy <willyhome85@gmail.com>
2018-09-28 19:32:34 +0800

    Largest Increasing Subsequence

--- CPE/LIS.cpp
+++ CPE/LIS.cpp
@@ -9,17 +9,17 @@ int lis(int arr[], int n)
     int lis[65535];
     int longest = 0;
 
-    for(int i=0; i<n; ++i)
+    for(int i=0; i<n; ++i) // initial 
         lis[i] = 1;
 
-    for(int i=0; i<n; ++i)
+    for(int i=0; i<n; ++i) // i: all j: ~i
     {
-
         for(int j=0; j<i; ++j)
         {
             if(arr[i] > arr[j] && lis[j]+1 > lis[i] )
             {
-                lis[i] = lis[j]+1;
+                lis[i] = lis[j]+1; // arr for check whether increasing
+                // lis for cnt the current length
             }
         }
     }
@@ -27,7 +27,7 @@ int lis(int arr[], int n)
     for(int i=0; i<n; ++i)
     {
         if(longest < lis[i])
-            longest = lis[i];
+            longest = lis[i]; // find the longest length
     }
 
     return longest;
