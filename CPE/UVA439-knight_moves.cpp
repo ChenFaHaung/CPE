26d5a63e77531dbc012add1babe47e764a4b0a06
Willy <willyhome85@gmail.com>
2018-09-28 19:55:26 +0800

    BFS, queue, struct, string2num

--- CPE/UVA439-knight_moves.cpp
+++ CPE/UVA439-knight_moves.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-typedef struct pointer_to_binary_function{
-    int x,y,step;
-}Point;
+typedef struct pointerto_binary_function{
+    int x,y,step; // x,y: position
+}Point; // struct Point constructor
 
-int pos[8][2] = {
+int pos[8][2] = { // moving ways
     {1,-2},
     {-1,-2},
     {1,2},
@@ -25,11 +25,11 @@ int BFS(Point s, Point d)
     if(s.x == d.x && s.y == d.y)
     {
         return s.step;
-    }
+    } // src meets dest
     
-    Point now;
-    bool isvis[8][8] = {false};
-    queue<Point> q;
+    Point now; // current condition
+    bool isvis[8][8] = {false}; // walking through record
+    queue<Point> q; // queue!
     q.push(s);
 
     while(!q.empty())
@@ -41,18 +41,18 @@ int BFS(Point s, Point d)
 
         for(int i=0; i<8; ++i)
         {
-            next.x = now.x + pos[i][0];
+            next.x = now.x + pos[i][0]; // every possible way
             next.y = now.y + pos[i][1];
-            next.step = now.step + 1;
+            next.step = now.step + 1; // move
 
             if((next.x<8 && next.x>=0) && (next.y<8 && next.y>=0) && !isvis[next.x][next.y])
-            {
+            { // in the range and never visit
                 isvis[next.x][next.y] = true;
                 if(next.x == d.x && next.y == d.y)
                 {
-                    return next.step;
+                    return next.step; // until meet the dest.
                 }
-                q.push(next);
+                q.push(next); // no meet queue the next step
             }
         }
     }
@@ -67,8 +67,8 @@ int main()
 
     while(cin >> str1 >> str2)
     {
-        src.step = 0;
-        src.x = str1[0] - 'a';
+        src.step = 0; // initail step
+        src.x = str1[0] - 'a'; // str to number
         src.y = str1[1] - '1';
         des.x = str2[0] - 'a';
         des.y = str2[1] - '1';
