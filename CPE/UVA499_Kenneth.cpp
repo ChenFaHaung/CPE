aeae5d1c5ea4d61f50ade8d2f49cb37598236f31
Willy <willyhome85@gmail.com>
2018-09-28 20:00:15 +0800

    memset, ASCII to index, cnting

--- CPE/UVA499_Kenneth.cpp
+++ CPE/UVA499_Kenneth.cpp
@@ -12,23 +12,23 @@ int main(void) {
 	memset(n, 0, sizeof(n));
 	while(scanf("%c", &c) != EOF) {
 		if(c != '\n')
-			n[c]++;
+			n[c]++; // cnt time of each char they show up 
 		else {
 			ans=0;
-			for(d = 'A'; d <= 'Z'; d++)
+			for(d = 'A'; d <= 'Z'; d++) // ASCII code for index
 				if(n[d] > ans)
-					ans = n[d];
+					ans = n[d]; // record the largest one
 			for(d = 'a'; d <= 'z'; d++)
 				if(n[d] > ans)
 					ans = n[d];
 			for(d = 'A'; d <= 'Z'; d++)
 				if(n[d] == ans)
-					printf("%c", d);
+					printf("%c", d); // find the largest
 			for(d = 'a'; d <= 'z'; d++)
 				if(n[d] == ans)
 					printf("%c", d);
 			printf(" %d\n", ans);
-			memset(n, 0, sizeof(n));
+			memset(n, 0, sizeof(n)); // clean the array
 		}
 	}
 	return 0;
