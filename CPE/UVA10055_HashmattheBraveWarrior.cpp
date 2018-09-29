882572181475050c18dc05e0ac572e85df57c2c3
Willy <willyhome85@gmail.com>
2018-09-29 21:36:11 +0800

    long long int, abs

--- CPE/UVA10055_HashmattheBraveWarrior.cpp
+++ CPE/UVA10055_HashmattheBraveWarrior.cpp
@@ -6,17 +6,14 @@ using namespace std;
 int main()
 {
     long long int a, b;
-    while(scanf("%lld%lld", &a, &b) != EOF)
+    while(scanf("%lld%lld", &a, &b) != EOF) // two long long minus
     {
         long long int ans = 0;
         if(a > b)
-        {
             ans = a - b;
-        }
         else
-        {
             ans = b - a;
-        }
+
         cout << ans << endl;
     }
     return 0;
