452c39d157b5285f5247a3d9c8a454c56138a78e
Willy <willyhome85@gmail.com>
2018-09-29 21:31:13 +0800

    strcmp, strlen, strcpy, char2-dim array

--- CPE/UVA630_Anagrams.cpp
+++ CPE/UVA630_Anagrams.cpp
@@ -11,7 +11,7 @@ int main(void)
     int t, i;
     int num = 0;
     char word[1010][30], temp[1010][30], test[30], temp2[30];
-
+    // string word[1001], tmp[1001], test, tmp2
     scanf("%d", &t);
     while(t--)
     {
@@ -19,11 +19,12 @@ int main(void)
         for(int j=0; j<i; j++)
         {
             scanf("%s", &word[j]);
-            strcpy(temp[j], word[j]);
+            strcpy(temp[j], word[j]); // copy to temp for sort but remain origin
+            // tmp[j] = word[j];
             sort(&temp[j][0], &temp[j][0]+strlen(temp[j]));
-        }
+        } // sort(tmp.begin(), tmp.end());
 
-        while(scanf("%s", &test) && test[0] != 'E')
+        while(scanf("%s", &test) && test[0] != 'E') // When END break
         {
             cout << "Anagrams for: " << test << endl;
             strcpy(temp2,test);
@@ -31,22 +32,17 @@ int main(void)
             num = 0;
             for(int j=0; j<i; j++)
             {
-                if(strcmp(temp[j],temp2) == 0)
+                if(strcmp(temp[j],temp2) == 0) // temp2 == temp[j]
                 {
                     num++;
                     cout << num << ") " << word[j] << endl;
                 }
             }
             if(num == 0)
-            {
                 cout << "No anagrams for: " << test << endl;
-            }
-
         }
         if(t)
-        {
             cout << "\n";
-        }
     }
 
     return 0;
