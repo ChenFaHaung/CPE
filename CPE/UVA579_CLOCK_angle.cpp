d2ff6a8cf309bfed07c7fc30d4631c95835501a8
Willy <willyhome85@gmail.com>
2018-09-29 21:34:53 +0800

    %.3lf, clock angle cal, >360, <0

--- CPE/UVA579_CLOCK_angle.cpp
+++ CPE/UVA579_CLOCK_angle.cpp
@@ -12,25 +12,19 @@ double clo_h, clo_m, ans;
 while(scanf("%d:%d", &h, &m) && (h | m))
 {
     if(h == 12)
-    {
         h = 0;
-    }
-    clo_h = 30*h + 0.5*m;
-    clo_m = 6*m;
+    
+    clo_h = 30*h + 0.5*m; // 0.5 degree/minute and 30 degree/hour
+    clo_m = 6*m; // 6 degree/minute
     ans = clo_h - clo_m;
 
     while(ans < 0)
-    {
         ans += 360;
-    }
 
     if(ans > 180)
-    {
         ans = 360 - ans;
-    }
-    printf("%.3lf\n", ans);
+
+    printf("%.3lf\n", ans); // 0.00x
 }
-
 return 0;
-
 }
