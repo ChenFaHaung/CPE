785aaa79d8c69397637208f00ce80d59b944f0f6
Willy <willyhome85@gmail.com>
2018-09-29 13:18:13 +0800

    Fibonacci array, binary base change

--- CPE/UVA948_FibonaccimalBase.cpp
+++ CPE/UVA948_FibonaccimalBase.cpp
@@ -9,13 +9,12 @@ int in_num;
 int f_arr[40];
 
 cin >> in_num;
-f_arr[0] = 1;
+
+f_arr[0] = 1; // fibonacci first
 f_arr[1] = 2;
 
-for(int i=2; i<40; i++)
-{
+for(int i=2; i<40; i++) // build the fibonacci array base
     f_arr[i] = f_arr[i-1] + f_arr[i-2];
-}
 
 while(in_num--)
 {
@@ -26,21 +25,17 @@ while(in_num--)
 
     for(int i=39; i>=0; i--)
     {
-        if(c >= f_arr[i])
+        if(c >= f_arr[i]) // if match print 1
         {
             cout << "1";
             c -= f_arr[i];
-            check = 1;
+            check = 1; // first digit
         }
         else if(check && c < f_arr[i])
-        {
             cout << "0";
-        }
     }
     cout << " (fib)" << endl;
-
 }
-
 return 0;
 
 }
