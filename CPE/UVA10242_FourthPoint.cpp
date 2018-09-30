cba5eeab0794007617907ba819399887a4a9af96
Willy <willyhome85@gmail.com>
2018-09-30 13:58:53 +0800

    First xy + second xy = third + fourth

--- CPE/UVA10242_FourthPoint.cpp
+++ CPE/UVA10242_FourthPoint.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 int main() {
 	double x1, y1, x2, y2, x3, y3, x4, y4;
 	while(scanf("%lf %lf %lf %lf %lf %lf %lf %lf", 
 				&x1, &y1, &x2, &y2, &x3, &y3, &x4, &y4) == 8) {
-		if(x1 == x3 && y1 == y3)
+		if(x1 == x3 && y1 == y3) // the same position is the third edge
 			printf("%.3lf %.3lf\n", (x2+x4)-x1, (y2+y4)-y1);
 		else if(x1 == x4 && y1 == y4)
 			printf("%.3lf %.3lf\n", (x2+x3)-x1, (y2+y3)-y1);
