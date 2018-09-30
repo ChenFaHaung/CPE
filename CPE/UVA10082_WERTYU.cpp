d0a707eccda5db4c0a04a304b6244c7137bb0013
Willy <willyhome85@gmail.com>
2018-09-30 13:47:42 +0800

    Keyboard shift, getchar, %c

--- CPE/UVA10082_WERTYU.cpp
+++ CPE/UVA10082_WERTYU.cpp
@@ -11,24 +11,18 @@ int main()
 
     while(c = getchar()) // char analysis
     {
-        if (c == EOF ) break;
+        if (c == EOF) break;
+
         int i=0;
-        //cout << c;
         if(c == ' ' || c == '\n') // space and line space
-        {
             printf("%c",c);
-        }
         else
         {
             while(str[i] != c) // scan the str until matching c
-            {
                 ++i;
-            }       
 
             printf("%c",str[i-1]); // print the one before str
         }
-        
     }
-    
     return 0;
 }
