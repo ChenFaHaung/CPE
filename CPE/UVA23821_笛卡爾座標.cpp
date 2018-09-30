32e6c87ea945fe6cbcc7415dbe2f97bc470f2e32
Willy <willyhome85@gmail.com>
2018-09-30 19:28:11 +0800

    Coordinate cal.

--- CPE/UVA23821_笛卡爾座標.cpp
+++ CPE/UVA23821_笛卡爾座標.cpp
@@ -9,12 +9,13 @@ int start[5][5];
 int start_add, goal_add, layer, total;
 scanf("%d", &n);
 count_n = n;
-while(n >= 1 && n <= 500){
+while(n >= 1 && n <= 500)
+{
     start_add = 0;goal_add = 0;layer = 0;total = 0;answer = 0;
-    for(int i = 0; i<2; i++){
-        for(int j = 0; j < 2; j++){
+    for(int i = 0; i<2; i++)
+    {
+        for(int j = 0; j < 2; j++)
             scanf("%d", &start[i][j]);
-        }
     }
     start_add = start[0][0] + start[0][1];
     goal_add = start[1][0] + start[1][1];
@@ -23,11 +24,11 @@ while(n >= 1 && n <= 500){
 
     total += layer;
 
-    for(int i = start_add+1 ; i < goal_add ; i++){
+    for(int i = start_add+1 ; i < goal_add ; i++)
         total += i;
-    }
-    total = (start_add - start[0][0]) + (goal_add- start[1][1]) + total;
-    answer = count_n-n+1;
+
+    total = (start_add - start[0][0]) + (goal_add - start[1][1]) + total;
+    answer = count_n - n + 1;
     printf("Case %d: %d\n", answer,total);
     n--;
 }
