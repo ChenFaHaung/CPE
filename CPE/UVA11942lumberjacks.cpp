044b8f3e7f1409e4cf1059d9bd3675c95c2a0982
Willy <willyhome85@gmail.com>
2018-09-30 19:38:12 +0800

    Incresing and decreasing

--- CPE/UVA11942lumberjacks.cpp
+++ CPE/UVA11942lumberjacks.cpp
@@ -16,64 +16,44 @@ int main(void)
     for(int i=0; i<input_line; i++)
     {
         if(input_line>20 || input_line<=0)
-        {
             break;
-        }
+
         int check = 0;
         int input_arr[10];
         int count_num = 0;
 
         for(int j=0; j<10; j++)
-        {
             cin >> input_arr[j];
-        }
-        if(input_arr[0] < input_arr[1])
-        {
+
+        if(input_arr[0] < input_arr[1]) // check increasing
             check = 0;
-        }
-        else
-        {
+        else // decreasing
             check = 1;
-        }
+        
         for(int j=0; j<9; j++)
         {
             if(check==0 && (input_arr[j] < input_arr[j+1]))
-            {
                 count_num++;
-            }
             else if(check==1 && (input_arr[j] > input_arr[j+1]))
-            {
                 count_num++;
-            }
             else
-            {
                 count_num = 0;
-            }
         }
 
         if(count_num == 9)
-        {
             mm[i] = 1;
-        }
         else
-        {
             mm[i] = 0;
-        }
     }
 
     cout << endl << "Lumberjacks:" << endl;
     for(int i=0; i<input_line; i++)
     {
         if(mm[i] == 1)
-        {
             cout << "Ordered" << endl;
-        }
         else
-        {
             cout << "Unordered" << endl;
-        }
     }
-
     return 0;
 }
 
