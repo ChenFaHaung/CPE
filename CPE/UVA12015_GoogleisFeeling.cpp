33d8c24d561d0010bd396c0ab10ade0ff11c61ea
Willy <willyhome85@gmail.com>
2018-09-30 19:32:45 +0800

    Find the max number' s string

--- CPE/UVA12015_GoogleisFeeling.cpp
+++ CPE/UVA12015_GoogleisFeeling.cpp
@@ -15,20 +15,20 @@ int main()
             int m = i+1;
             string str[10];
             int b[10], Max = 0;
+
             for(int j=0; j<10; ++j)
             {
                 cin >> str[j] >> b[j];
                 if(b[j] > Max)
-                    Max = b[j];
+                    Max = b[j]; // find the max
             }
             cout << "Case #" << m++ << ":\n";
             for(int j=0; j<10; ++j)
             {
-                if(b[j] == Max)
-                {
+                if(b[j] == Max) // print the max number string
                     cout << str[j] << endl;
-                }
             }
         }
     }
+    return 0;
 }
