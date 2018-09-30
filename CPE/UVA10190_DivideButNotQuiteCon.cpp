43187859843b3ea9377b3dde866548f81314d60c
Willy <willyhome85@gmail.com>
2018-09-30 12:47:08 +0800

    Devide, vector, %==0

--- CPE/UVA10190_DivideButNotQuiteCon.cpp
+++ CPE/UVA10190_DivideButNotQuiteCon.cpp
@@ -19,18 +19,17 @@ int main()
         {
             d.push_back(a);
 
-            if(a%b != 0)
+            if(a%b != 0) // no divide match
             {
                 flag = 1;
                 break;
             }
             else if(a/b == 1 && a%b == 0)
-            {
-                //a /= b;
+            { // the last one
                 d.push_back(1);
                 break;
             }
-            else
+            else // each miltiple is divede
                 a /= b;
         }
 
@@ -38,9 +37,7 @@ int main()
         else
         {
             for(int i=0; i<d.size()-1; ++i)
-            {
                 cout << d[i] << " ";
-            }
             cout << d[d.size()-1] << endl;
         }
     }
