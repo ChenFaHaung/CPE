0b5ff0d67bc3199e15f5d2eb9eaf7b68e19f09cd
Willy <willyhome85@gmail.com>
2018-09-30 13:53:02 +0800

    degree min, degree < 180, cal.

--- CPE/UVA10221_Satellites.cpp
+++ CPE/UVA10221_Satellites.cpp
@@ -8,12 +8,12 @@ int main()
 {
     double s, a;
     string str;
-    const double pi = acos(-1);
+    const double pi = acos(-1); // for pi 3.1415926
     while(cin >> s >> a >> str)
     {
-        if(str[0] == 'm'){a/=60;}
-        if(a>180){a = 360 - a;}
-        s += 6440;
+        if(str[0] == 'm'){a/=60;} // 60 degree = 1 min
+        if(a>180){a = 360 - a;} // a>180 360-a
+        s += 6440; // the earth radian
         printf("%.6lf %.6lf\n", s*pi*a/180.0, 2*s*sin(a*pi/180.0/2.0)); 
     }
     return 0;
