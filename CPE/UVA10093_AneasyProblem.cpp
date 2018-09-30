f0d2b74f3c86735d5f225cf11d51b809d15a0c44
Willy <willyhome85@gmail.com>
2018-09-30 13:45:46 +0800

    Digits sum % N-1 is N base

--- CPE/UVA10093_AneasyProblem.cpp
+++ CPE/UVA10093_AneasyProblem.cpp
@@ -13,15 +13,15 @@ int main()
         for(int i=0; i<str.length(); ++i)
         {
             if(str[i] >= 'A' && str[i] <= 'Z')
-                num = str[i] - 'A' + 10;
+                num = str[i] - 'A' + 10; // after 10 numbers
             else if(str[i] >= 'a' && str[i] <= 'z')
-                num = str[i] - 'a' + 36;
+                num = str[i] - 'a' + 36; // after 10 numbers and 26 chars
             else if(str[i] >= '0' && str[i] <='9')
                 num = str[i] - '0';
             else 
-                continue;
+                continue; // if not number
 
-            if(num > maxN)
+            if(num > maxN) // find the maximum num 
                 maxN = num;
             sum += num;
         }
@@ -29,9 +29,9 @@ int main()
         if(!maxN)
             maxN = 1;
         
-        for(int i=maxN; i<62; ++i)
+        for(int i=maxN; i<62; ++i) // from maxN for no need cal.
         {
-            if(sum%i == 0)
+            if(sum%i == 0) // digit sum % (n-1) -> n-based
             {
                 ans = i + 1;
                 break;
