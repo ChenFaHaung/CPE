58262e246940bc5ac8f6e0dc08c19c1948787ca4
Willy <willyhome85@gmail.com>
2018-10-01 13:58:49 +0800

    strcpy, strcat, strcmp

--- CPE/UVA11233_DeliDeli.cpp
+++ CPE/UVA11233_DeliDeli.cpp
@@ -12,9 +12,7 @@ int main(void)
 
     cin >> N >> L;
     for(int i=0; i<N; i++)
-    {
-        cin >> irreg[i][0] >> irreg[i][1];
-    }
+        cin >> irreg[i][0] >> irreg[i][1]; // dictionary
 
     int isIrreg = 0;
 
@@ -24,7 +22,7 @@ int main(void)
         cin >> word;
         for(int j=0; j<L; j++)
         {
-            if(!strcmp(word, irreg[j][0]))
+            if(!strcmp(word, irreg[j][0])) // non rule
             {
                 cout << irreg[j][1] << endl;
                 isIrreg = 1;
@@ -33,17 +31,15 @@ int main(void)
         }
 
         if(isIrreg == 1)
-        {
             continue;
-        }
 
         int length = strlen(word);
         char output[20] = "";
 
         if(word[length-1] == 'y' && (word[length-2] != 'a' && word[length-2] != 'e' && word[length-2] != 'i' && word[length-2] != 'o' && word[length-2] != 'u'))
         {
-            strncpy(output, word, length-1);
-            strcat(output,"ies");
+            strncpy(output, word, length-1); // output = word[length -1];
+            strcat(output,"ies"); // output += "ies";
             cout << output << endl;
         }
         else if(word[length-1] == '0' || word[length-1] == 's' || word[length-1] == 'x')
