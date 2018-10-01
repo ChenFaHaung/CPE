9869095764bfbea5bfd5f34bd416fab18852ec2d
Willy <willyhome85@gmail.com>
2018-10-01 14:20:04 +0800

    Fast Fabonacci

--- CPE/UVA11040_Brick Wall Patterns.cpp
+++ CPE/UVA11040_Brick Wall Patterns.cpp
@@ -3,25 +3,28 @@
 
 using namespace std;
 
-long long int fib(int n) {
+long long int fib(int n) 
+{
     if(n==1 || n==0)
         return 1;
     return fib(n-1) + fib(n-2);
 }
-int main(){
-int num_brick=0;
-long long int answer[52]={0};
-answer[0] = answer[1] = 1;
-    for(int i=2; i<52; i++){
+
+int main()
+{
+    int num_brick=0;
+    long long int answer[52]={0};
+    answer[0] = answer[1] = 1;
+
+    for(int i=2; i<52; i++)
        answer[i] = answer[i-2] + answer[i-1];
+    
+    while(scanf("%d", &num_brick) != EOF)
+    {
+        if(num_brick <= 0 || num_brick >50)
+            break;
+
+        printf("%lld\n", fib(num_brick));
     }
-while(scanf("%d", &num_brick) != EOF ){
-
-    if(num_brick <= 0 || num_brick >50)
-        break;
-
-    printf("%lld\n", fib(num_brick));
-}
-
 return 0;
 }
