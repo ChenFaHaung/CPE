b5e1eacc5fcf1fb2b642bc011c1ba21132e87fb0
Willy <willyhome85@gmail.com>
2018-10-01 16:02:35 +0800

    for index change

--- CPE/UVA10908_LargestSquare.cpp
+++ CPE/UVA10908_LargestSquare.cpp
@@ -8,9 +8,7 @@ bool check(char mp[101][101], char c, int s_r, int e_r, int s_c, int e_c, int in
     for( ; s_r <= e_r && s_c <= e_c; s_r +=inc_r, s_c += inc_c)
     {
         if(mp[s_r][s_c] != c)
-        {
             return false;
-        }
     }
     return true;
 }
@@ -28,14 +26,14 @@ int main()
             cin >> M >> N >> Q;
             cout << endl << M << " " << N << " " << Q << endl; 
 
-            char plate[101][101] = {0};
-            for(int i=0; i<M; ++i)
+            char plate[101][101] = {0}; // initial the square
+
+            for(int i=0; i<M; ++i) // input the data
             {
               for(int j=0; j<N; ++j)
-                {
                     cin >> plate[i][j];
-                }
             }
+
             int s,d;
             for(int i=0; i<Q; ++i)
             {
@@ -45,30 +43,20 @@ int main()
                 while(true)
                 {
                     if(!check(plate, plate[s][d], s-squ, s-squ, d-squ, d+squ, 0, 1))
-                    {
                         break;
-                    }    
                     if(!check(plate, plate[s][d], s+squ, s+squ, d-squ, d+squ, 0, 1))
-                    {
                         break;
-                    }
                     if(!check(plate, plate[s][d], s-squ, s+squ, d-squ, d-squ, 1, 0))
-                    {
                         break;
-                    }
                     if(!check(plate, plate[s][d], s-squ, s+squ, d+squ, d+squ, 1, 0))
-                    {
                         break;
-                    }
 
                     ++squ;
                 }
                 --squ;
-                cout << squ *2 +1 << endl;
-
+                cout << squ * 2 +1 << endl;
             }
         }
     }
-
     return 0;
 }
