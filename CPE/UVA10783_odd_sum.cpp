aa4993a3723b1e056174ab68a2fe7fe56d02a5ca
Willy <willyhome85@gmail.com>
2018-10-01 16:19:47 +0800

    sum of odd number in a range

--- CPE/UVA10783_odd_sum.cpp
+++ CPE/UVA10783_odd_sum.cpp
@@ -13,14 +13,12 @@ cin >> i;
 for(int j=1; j<=100; j++)
 {
     if(j%2 == 1)
-    {
         inte[j] = j;
-    }
     inte[j] += inte[j-1];
 }
 
 num = 0;
-while(num++ < i)
+/*while(num++ < i)
 {
     cin >> start >> endd;
 
@@ -30,9 +28,16 @@ while(num++ < i)
     }
 
     cout << "Case " << num << ": " << inte[endd] - inte[start - 1] << endl;
+}*/
+
+for(int j=0; j<i; ++j)
+{
+    num = 0;
+    cin >> start >> endd;
+
+    for(int k=start; k<=endd; ++k)
+        if(k%2){num += k;}
+        cout << "Case " << j+1 << ": " << num << endl;
 }
-
-
 return 0;
-
 }
