b82228231a5c3bfdcd76c42bb56eb1cf447cceb8
Willy <willyhome85@gmail.com>
2018-10-01 16:22:29 +0800

    i=p, j-1, <=m && <=i

--- CPE/UVA10721_BarCodes.cpp
+++ CPE/UVA10721_BarCodes.cpp
@@ -10,24 +10,21 @@ int main()
     while(cin >> n >> k >> m)
     {
         if(n<1 || m<1 || k<1 || n>50 || m>50 || k>50)
-        {
             break;
-        }
 
         long long int BC[60][60] = {0};
 
         BC[0][0] = 1;
 
-        for(int i=1; i<=n; ++i){
-            for(int j=1; j<=k; ++j){
-                for(int p=1; p<=m && p<=i; ++p){
+        for(int i=1; i<=n; ++i)
+        {
+            for(int j=1; j<=k; ++j)
+            {
+                for(int p=1; p<=m && p<=i; ++p)
                     BC[i][j] += BC[i-p][j-1];
-                }
             }
         }
-
         cout << BC[n][k] << endl;
     }
-
     return 0;
 }
