7a529420ba451ed6cd5a6a4ef2d0692f1849ef88
Willy <willyhome85@gmail.com>
2018-10-04 21:53:58 +0800

    b & (b-1) calculate time

--- CPE/d.c
+++ CPE/d.c
@@ -6,8 +6,16 @@ int mark_fifth_bit(int num)
 }
 int main()
 {
-    int a = 1;
+    int a = 4;
     printf("%d\n", mark_fifth_bit(a));
+    printf("%d\n", a);
 
+    int cnt = 0, b = 4;
+    while(b)
+    {
+        cnt++; b = b & (b-1);
+        printf("%d\t", b);
+    }
+    printf("%d\n", cnt);
     return 0;
 }
